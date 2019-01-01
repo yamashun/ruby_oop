fe0b617b86b6411a57bae0170a280a38aa4c8061
山田 俊輔 <yshunske@gmail.com>
2019-01-01 23:16:38 +0900

    外部のインターフェースのインスタンス初期化をwrapperクラスを用意することでインターフェースを改善するような方法もある

---
+++ 03_dependency/gear_wrapper.rb
@@ -0,0 +1,50 @@
+module SomeFrameWork
+  class Gear
+    attr_reader :chainring, :cog, :wheel
+
+    def initialize(chainring, cog, wheel)
+      @chainring = chainring
+      @cog = cog
+      @wheel = wheel
+    end
+
+    def ratio
+      chainring / cog.to_f
+    end
+
+    def gear_inches
+      ratio * diameter
+    end
+
+    def diameter
+      wheel.diameter
+    end
+  end
+end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+
+module GearWrapper
+  def self.gear(args)
+    SomeFrameWork::Gear.new(args[:chainring], args[:cog], args[:wheel])
+  end
+end
+
+
+puts GearWrapper.gear(chainring: 52, cog: 11, wheel: Wheel.new(26, 1.5)).gear_inches
