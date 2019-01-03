0241d37617e5ed1fc601bf1948aa7aee054a59bd
山田 俊輔 <yshunske@gmail.com>
2019-01-03 22:10:49 +0900

    tripクラス追加 ダックタイプを使う前の状態

---
+++ 05_duck_typing/trip.rb
@@ -0,0 +1,20 @@
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(mechanic)
+    mechanic.prepare_bicycles(bicycles)
+  end
+
+  # ...
+end
+
+
+class Mechanic
+  def prepare_bicycles(bicycles)
+    bicycles.each{|bicycle| prepare_bicycle(bicycle)}
+  end
+
+  def prepare_bicycle(bicycle)
+    # ...
+  end
+end
