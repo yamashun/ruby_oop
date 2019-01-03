0b10ae9eedcd80dc0f9d23da4372dd00e3f01e91
山田 俊輔 <yshunske@gmail.com>
2019-01-03 22:19:18 +0900

    prepareに渡されるクラスの種類を追加。単純に条件分岐して処理を分けた場合

--- 05_duck_typing/trip.rb
+++ 05_duck_typing/trip.rb
@@ -1,8 +1,18 @@
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
-  def prepare(mechanic)
-    mechanic.prepare_bicycles(bicycles)
+  def prepare(prepares)
+    prepares.each do |prepare|
+      case prepare
+      when Mechanic
+        prepare.prepare_bicycle(bicycle)
+      when TripCoordinator
+        prepare.buy_food(customers)
+      when Driver
+        prepare.gas_up(vehicle)
+        prepare.fill_water_tank(vehicle)
+      end
+    end
   end
 
   # ...
@@ -15,6 +25,23 @@ class Mechanic
   end
 
   def prepare_bicycle(bicycle)
+    # ...
+  end
+end
+
+
+class TripCoordinator
+  def buy_food(customers)
+    # ...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    # ...
+  end
+
+  def fill_water_tank(vehicle)
     # ...
   end
 end
