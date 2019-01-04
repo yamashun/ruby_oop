009e4dbde09412dce528af0ade36fd4124fba477
山田 俊輔 <yshunske@gmail.com>
2019-01-04 22:02:56 +0900

    Bicyleを変更 アンチパターン 複数の自転車に対応する修正を入れたが、種類によってどの部品が初期化されているかがわかりづらい。更に種類が増えるともっとわかりづらくなる。

--- 06_inheritance/bicycle.rb
+++ 06_inheritance/bicycle.rb
@@ -1,25 +1,38 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      {
+        chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color
+      }
+    else
+      {
+        chain: '10-speed',
+        tire_size: '2.1',
+        rear_shock: rear_shock
+      }
+    end
   end
 end
 
 bike = Bicycle.new(
+  style: 'mountain',
   size: 'M',
-  tape_color: 'red'
+  tape_color: 'red',
+  front_shock: 'Maintou',
+  rear_shock: 'Fox'
 )
 
-puts bike.size
 puts bike.spares
 
