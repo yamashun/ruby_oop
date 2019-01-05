7af740f6a12283c13678a1fb76047e5d21b79d8e
山田 俊輔 <yshunske@gmail.com>
2019-01-05 15:21:04 +0900

    単純にBicycleを継承する。MountainBikeインスタンスがロードバイクとマウンテンバイクの振る舞いをごちゃまぜに含んでしまっている

--- 06_inheritance/bicycle.rb
+++ 06_inheritance/bicycle.rb
@@ -15,6 +15,20 @@ class Bicycle
   end
 end
 
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
 bike = Bicycle.new(
   size: 'M',
   tape_color: 'red'
@@ -23,3 +37,11 @@ bike = Bicycle.new(
 puts bike.size
 puts bike.spares
 
+mountain_bike = MountainBike.new(
+  size: 'S',
+  front_shock: 'Maintou',
+  rear_shock: 'Fox'
+)
+
+puts mountain_bike.size
+puts mountain_bike.spares
