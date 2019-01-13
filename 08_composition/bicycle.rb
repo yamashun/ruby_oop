c5ad97f15e731bfe407fe32a3b70f9c9ac25da16
山田 俊輔 <yshunske@gmail.com>
2019-01-13 11:41:22 +0900

    PartsFactory追加

--- 08_composition/bicycle.rb
+++ 08_composition/bicycle.rb
@@ -37,62 +37,26 @@ class Part
   end
 end
 
-# class RoadBikeParts < Parts
-#   attr_reader :tape_color
-
-#   def post_initialize(args)
-#     @tape_color = args[:tape_color]
-#   end
-
-#   def local_spares
-#     { tape_color: tape_color }
-#   end
-
-#   def default_tire_size
-#     '23'
-#   end
-# end
-
-# class MountainBikeParts < Parts
-#   attr_reader :front_shock, :rear_shock
-
-#   def post_initialize(args)
-#     @front_shock = args[:front_shock]
-#     @rear_shock = args[:rear_shock]
-#   end
-
-#   def local_spares
-#     { rear_shock: rear_shock }
-#   end
-
-#   def default_tire_size
-#     '2.1'
-#   end
-# end
-
-# road_bike = Bicycle.new(
-#   size: 'L',
-#   parts: RoadBikeParts.new(tape_color: 'red')
-# )
-# puts road_bike.size
-# puts road_bike.spares
-
-# mountain_bike = Bicycle.new(
-#   size: 'L',
-#   parts: MountainBikeParts.new(rear_shock: 'Fox')
-# )
-
-# puts mountain_bike.size
-# puts mountain_bike.spares
-
-
+module PartsFactory
+  def self.build(config, part_class = Part, parts_class = Parts)
+    parts_class.new(
+      config.collect{|part_config|
+        part_class.new(
+          name: part_config[0],
+          description: part_config[1],
+          needs_spare: part_config.fetch(2, true)
+        )
+      }
+    )
+  end
+end
 
 chain = Part.new(name: 'chain', description: '10-speed')
 road_tire = Part.new(name: 'tire_size', description: '23')
 tape = Part.new(name: 'tape_color', description: 'red')
 mountain_tire = Part.new(name: 'tire_size', description: '2.1')
 rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
-front_shock = Part.new(name: 'front_shock', description: 'Maintou', need_spare: false)
+front_shock = Part.new(name: 'front_shock', description: 'Maintou', needs_spare: false)
 
 road_bike_parts = Parts.new([chain, road_tire, tape])
 road_bike = Bicycle.new(size: 'L', parts: road_bike_parts)
@@ -110,3 +74,17 @@ puts mountain_bike.spares
 
 puts road_bike.parts.size
 puts mountain_bike.parts.size
+
+
+road_config = [
+  ['chain', '10-speed'], ['tire_size', '23'], ['tape_color', 'red']
+]
+
+mountain_config = [
+  ['chain', '10-speed'], ['tire_size', '2.1'], ['front_shock', 'Maintou', false], ['rear_shock', 'FOX']
+]
+
+road_parts = PartsFactory.build(road_config)
+puts road_parts
+mountain_parts = PartsFactory.build(mountain_config)
+puts mountain_parts
