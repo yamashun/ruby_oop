aa5b7ff4e33b8624f624670d1656728ee7bd8053
山田 俊輔 <yshunske@gmail.com>
2019-01-13 19:26:58 +0900

    gearとwheelのテスト追加

---
+++ 09_test/wheel_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require './wheel.rb'
+
+class WheelTest < MiniTest::Unit::TestCase
+  def test_calculate_diameter
+    wheel = Wheel.new(26, 1.5)
+
+    assert_in_delta(29, wheel.diameter, 0.01)
+  end
+end
+
+
+class GearTest < MiniTest::Unit::TestCase
+  def test_calculates_gear_inches
+    gear = Gear.new(
+      chainring: 52,
+      cog: 11,
+      rim: 26,
+      tire: 1.5
+    )
+
+    assert_in_delta(137.1, gear.gear_inches, 0.01)
+  end
+end
+
