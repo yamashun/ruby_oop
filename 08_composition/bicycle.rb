118ddfa36258d0eb07a74dbc83b44bf051943180
山田 俊輔 <yshunske@gmail.com>
2019-01-13 11:51:18 +0900

    OpendStructを使ってPartを表現するように修正

--- 08_composition/bicycle.rb
+++ 08_composition/bicycle.rb
@@ -27,53 +27,24 @@ class Parts
   end
 end
 
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
-
+require 'ostruct'
 module PartsFactory
-  def self.build(config, part_class = Part, parts_class = Parts)
+  def self.build(config, parts_class = Parts)
     parts_class.new(
       config.collect{|part_config|
-        part_class.new(
-          name: part_config[0],
-          description: part_config[1],
-          needs_spare: part_config.fetch(2, true)
-        )
+        create_part(part_config)
       }
     )
   end
+
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name: part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true)
+    )
+  end
 end
-
-chain = Part.new(name: 'chain', description: '10-speed')
-road_tire = Part.new(name: 'tire_size', description: '23')
-tape = Part.new(name: 'tape_color', description: 'red')
-mountain_tire = Part.new(name: 'tire_size', description: '2.1')
-rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
-front_shock = Part.new(name: 'front_shock', description: 'Maintou', needs_spare: false)
-
-road_bike_parts = Parts.new([chain, road_tire, tape])
-road_bike = Bicycle.new(size: 'L', parts: road_bike_parts)
-
-puts road_bike.size
-puts road_bike.spares
-
-mountain_bike = Bicycle.new(
-  size: 'L',
-  parts: Parts.new([chain, mountain_tire, front_shock, rear_shock])
-)
-
-puts mountain_bike.size
-puts mountain_bike.spares
-
-puts road_bike.parts.size
-puts mountain_bike.parts.size
 
 
 road_config = [
